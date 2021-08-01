757fcc834a27a4f856aa1310159f957571e3fc03
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2021-08-01 12:13:26 +1000

    Adding init project

---
+++ event-listner/src/main/java/org/kds/el/EventListnerApplication.java
@@ -0,0 +1,13 @@
+package org.kds.el;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventListnerApplication.class, args);
+	}
+
+}
