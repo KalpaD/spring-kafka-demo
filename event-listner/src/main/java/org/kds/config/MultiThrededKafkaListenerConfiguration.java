8acf0aeaf16ec77aefc6870107c3af8cd5b74ed0
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2021-08-01 16:25:24 +1000

    Adding a multi threaded consumer with exponential backoff and dead letter queue.

---
+++ event-listner/src/main/java/org/kds/config/MultiThrededKafkaListenerConfiguration.java
@@ -0,0 +1,81 @@
+package org.kds.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.kds.model.PaymentEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.ExponentialBackOff;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+@Configuration
+public class MultiThrededKafkaListenerConfiguration {
+
+    public ConsumerFactory<String, PaymentEvent> paymentConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(PaymentEvent.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> paymentKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(paymentConsumerFactory());
+        factory.setConcurrency(3);
+
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(paymentEventKafkaTemplateKafkaTemplate(),
+                (r, e) -> new TopicPartition(r.topic() + "-fail", r.partition()));
+
+        ExponentialBackOffWithMaxRetries exponentialBackOffWithMaxRetries = new ExponentialBackOffWithMaxRetries(3);
+        exponentialBackOffWithMaxRetries.setInitialInterval(1_000L);
+        exponentialBackOffWithMaxRetries.setMultiplier(2.0);
+
+        SeekToCurrentErrorHandler handler = new SeekToCurrentErrorHandler(recoverer, exponentialBackOffWithMaxRetries);
+        handler.addNotRetryableExceptions(IllegalArgumentException.class);
+
+        factory.setErrorHandler(handler);
+        return factory;
+    }
+
+
+    @Bean
+    public ProducerFactory<String, PaymentEvent> paymentEventProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentEvent> paymentEventKafkaTemplateKafkaTemplate() {
+        return new KafkaTemplate<>(paymentEventProducerFactory());
+    }
+
+}
